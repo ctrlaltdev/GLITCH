c5d97ae97c7959f2008bd8712eb3d9586c59eb72
ctrlaltdev <yorick@ctrlaltdev.xyz>
2018-02-14 00:10:07 +0100

    change to on-demand image generation

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ params.quality = 0.555;
 params.quant = 0.333;
 params.glitch = 0.333;
 params.active = true;
-params.download = false;
+params.download = true;
 
 var gui = new dat.GUI();
 gui.add( params, 'quality', 0.0, 1.0 );
@@ -27,27 +27,40 @@ gui.add( params, 'glitch', 0.0, 1.0 );
 gui.add( params, 'active' );
 gui.add( params, 'download' );
 
+var img;
+
 var firstLoad = true;
 image.onload = function(){
   if( firstLoad ){
-    var img = new Image();
+    img = new Image();
     document.body.appendChild( img );
-    var update = function(){
-      if ( params.active ) {
-        params.seed = Math.random();
-        jpgGlitch.glitch( image, params );
-        img.src = jpgGlitch.url;
-        if ( params.download ) {
-          var a = document.createElement( 'a' );
-          a.href = jpgGlitch.url;
-          a.download = 'download.jpg';
-          a.click();
-          params.download = false;
-        }
-      }
-      requestAnimationFrame( update );
-    };
     update();
     firstLoad = false;
   }
 };
+
+var update = function(){
+  if ( params.active ) {
+    params.seed = Math.random();
+    jpgGlitch.glitch( image, params );
+    img.src = jpgGlitch.url;
+    if ( params.download ) {
+      var a = document.createElement( 'a' );
+      a.href = jpgGlitch.url;
+      a.download = 'download.jpg';
+      a.click();
+      params.download = false;
+    }
+  }
+  //requestAnimationFrame( update );
+};
+
+var generate = document.createElement('button');
+generate.id = 'generate';
+var generateTxt = document.createTextNode('Generate');
+generate.appendChild(generateTxt);
+document.body.appendChild(generate);
+
+document.getElementById('generate').addEventListener('click', () => {
+  update();
+});
